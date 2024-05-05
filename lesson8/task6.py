6b3d08896519e203432bd9e207d7d1c49329843b
Daria <bezruchkodasha2107@gmail.com>
2024-05-05 18:36:24 +0300

    task6_8

--- lesson8/task6.py
+++ lesson8/task6.py
@@ -7,3 +7,19 @@ Yes - если число уже встречалось и No, если нет
 если в списке не все целые числа вернуть False.
 
 """
+def yes_or_no(l):
+    seem = {}
+    result = []
+
+    for num in l:
+        if not isinstance(num, int):
+            return False
+        if num in seem:
+            result.append('yes')
+        else:
+            result.append('no')
+            seem[num] = True
+    return result
+
+l = [1, 2, 3, 1, 4.2]
+print(yes_or_no(l))
