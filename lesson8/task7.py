1603b4fd3629d37beb0bac7182071a0600e69953
Daria <bezruchkodasha2107@gmail.com>
2024-05-05 19:04:44 +0300

    task7_8

--- lesson8/task7.py
+++ lesson8/task7.py
@@ -6,3 +6,31 @@
 и выводит его содержимое в удобном и красивом формате. 
 
 """
+def text_info(text):
+    num_chars = len(text)
+    num_words = len(text.split())
+    num_str = text.count('\n') + 1
+    num_sentences = text.count('.') + text.count('!') + text.count('?')
+
+    info = {
+        'Characters': num_chars,
+        'Words': num_words,
+        'Lines': num_str,
+        'Sentences': num_sentences
+    }
+    return info
+
+def print_text_info(info):
+    print("Text Info:")
+    for key, value in info.items():
+        print(f"{key}: {value}")
+
+text = """
+This is a sample text.
+It has multiple lines.
+It also contains multiple sentences! Right?
+And a few characters too...
+"""
+
+info = text_info(text)
+print_text_info(info)
