a5a21cf6cb62f5b5d89affae56d7012e2ce8485d
Daria <bezruchkodasha2107@gmail.com>
2024-05-21 00:11:58 +0300

    1_10

--- lesson10/task1.py
+++ lesson10/task1.py
@@ -4,11 +4,11 @@
 ключевых-слов.
 
 Если все позиционные аргументы - целые числа, то рассчитать их сумму. Если
-нет, то кинуть ошибку TypeError("Все позиционные аргументы должны быть целыми").
+нет, то кинуть ошибку TypeError(Все позиционные аргументы должны быть целыми).
 
 Если все именованные аргументы - ключевые слова являются строками, то найти максимальную
-длину слова. Если нет, то кинуть ошибку TypeError("Все аргументы - ключевые
-слова должны быть строками").
+длину слова. Если нет, то кинуть ошибку TypeError(Все аргументы - ключевые
+слова должны быть строками).
 
 Функция должна вернуть словарь, вида:
 {
@@ -16,3 +16,41 @@
     "kwargs_max_len": 7
 }
 """
+def dict_from_args(*args, **kwargs):
+
+    for arg in args:
+        if not isinstance(arg, int):
+            raise TypeError ("Все позиционные аргументы должны быть целыми")
+        
+    args_sum = sum(args)
+
+    for kwarg in kwargs.keys():
+        if not isinstance(kwarg, str):
+            raise TypeError("Все аргументы - ключевые слова должны быть строками")
+        
+    kwargs_max_len = max([len(key) for key in kwargs.keys()])
+
+    result = {
+        "args_sum": args_sum,
+        "kwargs_max_len": kwargs_max_len
+    }
+
+    return result
+
+try:
+    print(dict_from_args(1, 2, 3, 4, 5, a=9, aa=52, bbbbbbb=0))
+except TypeError as e:
+    print(e)
+
+try:
+    print(dict_from_args(1, 2, 3.5, 4, 5, a=9, aa=52, bbbbbbb=0))
+except TypeError as e:
+    print(e)
+    
+
+
+  
+
+
+
+    
