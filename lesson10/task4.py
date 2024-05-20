079cd7b6fc59c7f8e62d9a63d30dfa4ac9901184
Daria <bezruchkodasha2107@gmail.com>
2024-05-21 01:13:03 +0300

    4_10

--- lesson10/task4.py
+++ lesson10/task4.py
@@ -4,3 +4,19 @@
 а выводить имя функции в которой произошла ошибка и информацию об ошибке в консоль. 
 Имя функции можно узнать использовав свойство __name__ ( print(func.__name__) )
 """
+def not_error_decorator(f):
+    def wrapper(*args):
+        try:
+            print(f(*args))
+        except ZeroDivisionError as e:
+            print(f.__name__)
+        except TypeError as e:
+            print(f.__name__)
+            
+    return wrapper
+
+@not_error_decorator
+def numerical(a, b):
+    return a/b
+
+numerical(5, 'a')
